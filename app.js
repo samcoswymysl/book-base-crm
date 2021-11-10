b55ad3c871149984eb4af7c0de35deb23490a0f8
samcoswymysl <rafalkrukowskipraca123@gmail.com>
2021-11-10 15:43:55 +0100

    Build a project tree.
    Add style.css
    Add main.hbs
    Add home.js

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 
 const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.engine('.hbs', hbs({
+  extname: '.hbs',
+  // helpers: handlebarsHelpers,
+}));
+app.set('view engine', '.hbs');
+
+
+
+
+
 
 app.listen(3000, 'localhost', () => {
   console.log('Serwer listen on http://localhost:3000');
