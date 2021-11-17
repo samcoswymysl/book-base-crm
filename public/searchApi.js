f12953e9ed36cea1158ca46141daf71a3a0b5e04
samcoswymysl <rafalkrukowskipraca123@gmail.com>
2021-11-17 18:17:05 +0100

    Add filtr data from API and send data for backend

--- public/searchApi.js
+++ public/searchApi.js
@@ -1,13 +1,29 @@
-import { findBooksByTitle } from './apiUtils.js';
+import { findBooksByTitle, findCover } from './apiUtils.js';
 
 const button = document.querySelector('.findTitle');
 const input = document.querySelector('#title');
 
+const sendData = async (data) => {
+  await fetch('http://localhost:3000/home', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
 
+  });
+};
 
+const findImportantInfo = (docs) => {
+  const importantData = docs.map((doc) => ({
+    cover: findCover(doc),
+    authors: doc.author_name,
+    title: doc.title,
+  }));
+  sendData(importantData);
+};
 
 button.addEventListener('click', async () => {
   const dataFromApi = await findBooksByTitle(input.value);
+  await findImportantInfo(dataFromApi);
 });
-
-
