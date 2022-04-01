b309349bed44c6a5eda6a29080e45a2851426138
rafalkrukowski <90904361+samcoswymysl@users.noreply.github.com>
2022-04-01 19:37:09 +0200

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const passport = require('config/passport');
+const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const passportJWT = require('passport-jwt');
 const { compare } = require('bcrypt');
